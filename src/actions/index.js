bbcf6438cab44034092e6871cfdc3fdf887c0528
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-05 16:52:21 -0700

    Pass add trip action creator test.

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,16 @@ export const deleteTrip = id => ({
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
+
+export const addTrip = (trip) => {
+  const {destination, departureDate, returnDate, petName, notes, id} = trip;
+  return {
+    type: 'ADD_TRIP',
+      destination: destination,
+      departureDate: departureDate,
+      returnDate: returnDate,
+      petName: petName,
+      notes: notes,
+      id: id
+  }
+}
