9aadca7cdfda88704b26ca80f62672e1b97e51ce
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-05 16:45:22 -0700

    Fail toggleForm action creator test.

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -8,6 +8,10 @@ describe('MyTrip actions', () => {
     });
   });
 
-  
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
 
 });
