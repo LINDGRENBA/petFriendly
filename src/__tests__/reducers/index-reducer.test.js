0e33f1b0c1553d636372f3cb2d5640070557a115
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-05 16:06:16 -0700

    Pass test to check that rootReducer reflects changes after actions passed to reducers.

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -22,4 +22,26 @@ describe('rootReducer', () => {
     expect(store.getState().formVisible).toEqual(formVisibleReducer(undefined, {type:null}));
   });
 
+  test('check that initial state of tripListReducer matches root reducer', () => {
+    const action = {
+      type: 'ADD_TICKET',
+      destination: "Turkey",
+      departureDate: "2020-10-06",
+      returnDate: "2020-10-07",
+      petName: "Gemma",
+      notes: "Remember to bring Mr. Bear",
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().mainTripList).toEqual(tripListReducer(undefined, action));
+  });
+
+  test('check that initial state of formVisibleReducer matches root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisible).toEqual(formVisibleReducer(undefined, action));
+  });
+
 });
