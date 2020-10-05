ab6ccd65aff07704a8ece411109d8c8ab66abbd1
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-05 14:29:45 -0700

    Add import statements for Redux to index.js, wrap app component in provider component.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import reducer from './reducers/trip-list-reducer';
+import {Provider} from 'react-redux';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
