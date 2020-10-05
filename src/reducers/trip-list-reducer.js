fe8d5bd91ed794ef30bb278e6ee0a6c115e7dbe8
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-05 14:14:30 -0700

    Pass delete trip test.

--- src/reducers/trip-list-reducer.js
+++ src/reducers/trip-list-reducer.js
@@ -12,6 +12,10 @@ export default (state = {}, action) => {
           id: id
         }
       });
+      case 'DELETE_TRIP':
+        const newState = { ...state };
+        delete newState[id];
+        return newState;
     default:  
       return state;
   }
