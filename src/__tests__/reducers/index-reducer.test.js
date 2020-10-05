26732639ddd960134aff9ac0ccd57b7a989901d4
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-05 16:00:28 -0700

    Pass both smoke tests for root reducer.

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from '../../reducers/index';
+import {createStore} from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import tripListReducer from '../../reducers/trip-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe('rootReducer', () => {
 
@@ -7,6 +12,14 @@ describe('rootReducer', () => {
       mainTripList: {},
       formVisible: false
     });
+  });
+
+  test('check that initial state of tripListReducer matches root reducer', () => {
+    expect(store.getState().mainTripList).toEqual(tripListReducer(undefined, {type:null}));
+  });
+
+  test('check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisible).toEqual(formVisibleReducer(undefined, {type:null}));
   });
 
 });
