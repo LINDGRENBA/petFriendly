6ab054e51fc3037c2c55322cd542943884586157
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-06 22:42:58 -0700

    Add custom css file, import custom css file into App.js component.

--- src/components/App.js
+++ src/components/App.js
@@ -6,25 +6,28 @@ import TripControl from './TripControl';
 import SearchControl from './SearchControl'
 import Signin from './Signin';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './../custom.css';
 
 function App() {
   return (
-    <Router>
-      <NavMenu />
-      <h2>Welcome to Pet Friendly</h2>
-      <p>An app to take the stress out of traveling with your pet.</p>
-      <Switch>
-        <Route exact path="/">
-          <SearchControl />
-        </Route>
-        <Route exact path="/signin">
-          <Signin />
-        </Route>
-        <Route exact path="/mytrips">
-          <TripControl />
-        </Route>
-      </Switch>
-    </Router>
+    <div className="container">
+      <Router>
+        <NavMenu />
+        <h2>Welcome to Pet Friendly</h2>
+        <p>An app to take the stress out of traveling with your pet.</p>
+        <Switch>
+          <Route exact path="/">
+            <SearchControl />
+          </Route>
+          <Route exact path="/signin">
+            <Signin />
+          </Route>
+          <Route exact path="/mytrips">
+            <TripControl />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
