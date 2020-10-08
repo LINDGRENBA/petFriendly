a8f6a45f4b6b953a290d9e3c97b2ebed8b11cf6d
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-07 22:15:35 -0700

    Uninstall reactstrap and bootstrap, remove changes from NavMenu.

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -2,23 +2,24 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './NavMenu.css';
 
-function NavMenu() {
-    return (
-      <React.Fragment>
-        <h1 className="brand">PetFriendly</h1>
-        <ul>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/mytrips">MyTrips</Link>
-          </li>
-          <li>
-            <Link to="/">Sign In</Link>
-          </li>
-        </ul>
-      </React.Fragment>
-    );
-  }
+function NavMenu () {
+    
+  return (
+    <React.Fragment>
+      <h1 className="brand">PetFriendly</h1>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/mytrips">MyTrips</Link>
+        </li>
+        <li>
+          <Link to="/">Sign In</Link>
+        </li>
+      </ul>
+    </React.Fragment>
+  );
+}
 
 export default NavMenu; 
